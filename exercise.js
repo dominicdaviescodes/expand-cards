6c15e9415ed04e17b34690dd44f268fb9d43a15c
dominicdaviescodes <codewithdom@gmail.com>
2020-12-19 22:20:05 +0000

    add exercise script to markup

---
+++ exercise.js
@@ -0,0 +1,8 @@
+const panels = document.querySelectorAll('.panel');
+
+panels.forEach((panel) => {
+  // console.log(panel);
+  panel.addEventListener('click'){
+    classList.add('active');
+  }
+});
